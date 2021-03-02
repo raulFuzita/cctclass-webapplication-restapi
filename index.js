847e8624f6c37b849670bc2f75fd7e98ed0e4158
raulFuzita <raul_fuzita@hotmail.com>
2021-03-02 11:29:45 +0000

    Testing app, read instruction to send a parameter

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const http = require('http');
-const axios = require('axios');
+axios = require('axios'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parser');
 
-http.createServer((req, res)=>{
-  res.write(chars.join("\n")); //display the list of users on the page
-  res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+var app = express();
+var port = 8000;
 
-let chars = []; // names of users will be stored here
+// Pass the parameter: server-domain/hello/raul/chocolate
+app.get('/hello/:foo/:bar', (req, res) => {
+    res.json({message: 'Hello BScBest!', data: [
+        req.params.foo,
+        req.params.bar
+    ]});
+});
 
-(async function getNames(){
-  try{
-    const {data} = await axios.get("https://swapi.dev/api/people/");
-    chars = data.results.map(char=>char.name);
-    console.log(chars);
-  } catch(error){
-    console.log(error)
-  }
-})()
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
